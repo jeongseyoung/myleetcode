c3f207e609d68b896581203d6153cdd7896aeb11
jeongseyoung <sengangel@naver.com>
2024-05-31 14:10:23 +0900

    Update 3_Longest Substring Without Repeating Characters.py

--- 3_Longest Substring Without Repeating Characters.py
+++ 3_Longest Substring Without Repeating Characters.py
@@ -19,28 +19,16 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 def substg(s):
     #print(s.find("w"))
     mylist:list = [x for x in s]
-    #newlist:list = []
-    #print(mylist[1:])
     mydic:dict = {}
-    #print("index",mylist.index("d"))
-    #result:list = []
     start = 0
     max_len = 0
-    #temp = mylist[0]
-    #print(max(1,2))
-    #testdic = {"p":0,"w":1}
-    #print(testdic)
-    #rint(testdic["p"])
     for index,value in enumerate(mylist):
-        #if value in mydic and value != mylist[index-1]:
         if value in mydic:
                 start = max(start, mydic.get(value) + 1)
-                print(mydic, value, "start:", mydic.get(value), "+ 1 = ", start)
-        # elif value in mydic and value == mylist[index-1]:    
-        #     start = index 
+                print(mydic, value, "start:", mydic.get(value), "+ 1 = ", start)    
+                start = index 
             #print("start",start)    
-        mydic[value] = index
-        #print(mydic)
+        mydic[value] = index        
         print(index,start,index-start+1)
         max_len = max(max_len, index - start + 1) # index - start + 1 -> 길이구하기~
         print("max_len",max_len)
@@ -54,4 +42,3 @@ if __name__ == "__main__":
     mystr4 = "ckilbkd"
     mystr5 = "abba"
     print(substg(mystr5))
-    #print(substg(mystr1))
