695d154c902b57ea408888629a4f5449f6c9f2e5
jeongseyoung <sengangel@naver.com>
2025-05-25 23:55:33 +0900

    Update 8_String to Integer.py

--- 8_String to Integer.py
+++ 8_String to Integer.py
@@ -2,7 +2,7 @@
 Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
         
 The algorithm for myAtoi(string s) is as follows:
-
+    
 Whitespace: Ignore any leading whitespace (" ").
 Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity is neither present.
 Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
