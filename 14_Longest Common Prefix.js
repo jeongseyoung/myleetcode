30ec6acffed5234a89596b88a6103e175b8114af
jeongseyoung <sengangel@naver.com>
2025-06-22 23:57:40 +0900

    Update 14_Longest Common Prefix.js

--- 14_Longest Common Prefix.js
+++ 14_Longest Common Prefix.js
@@ -8,7 +8,7 @@ Output: "fl"
                     
 Example 2:
 Input: strs = ["dog","racecar","car"]
-Output: ""
+Output: ""   
 Explanation: There is no common prefix among the input strings
 */
        
