b3dcee87507feffb6ede8bc18c240baa91aaa5f6
jeongseyoung <sengangel@naver.com>
2025-07-17 23:59:24 +0900

    Update 14_Longest Common Prefix.js

--- 14_Longest Common Prefix.js
+++ 14_Longest Common Prefix.js
@@ -5,7 +5,7 @@ If there is no common prefix, return an empty string ""
 Example 1:
 Input: strs = ["flower","flow","flight"]
 Output: "fl"    
-                    
+                      
 Example 2:    
 Input: strs = ["dog","racecar","car"]
 Output: ""   
