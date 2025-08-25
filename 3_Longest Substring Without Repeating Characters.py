989eaed6f5a21ff02d05bf89b7c1c369862266ce
jeongseyoung <sengangel@naver.com>
2025-08-25 23:49:41 +0900

    Update 3_Longest Substring Without Repeating Characters.py

--- 3_Longest Substring Without Repeating Characters.py
+++ 3_Longest Substring Without Repeating Characters.py
@@ -4,7 +4,7 @@ Example 1:
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
-
+    
 Example 2:
 Input: s = "bbbbb"
 Output: 1
@@ -42,3 +42,4 @@ if __name__ == "__main__":
     mystr4 = "ckilbkd"
     mystr5 = "abba"
     print(substg(mystr5))
+
