35c4524108684762b228ac7eb0bc96f8e84c02c2
jeongseyoung <sengangel@naver.com>
2025-09-02 23:57:11 +0900

    Update 283_Move Zeroes.py

--- 283_Move Zeroes.py
+++ 283_Move Zeroes.py
@@ -10,7 +10,7 @@ cnt = 0
         for n in range(cnt):
             nums.append(0)
 """         
-"""                     
+"""                         
 0,1,0,3,12
 1 0 0 3 12    
 1 0 3 0 12  
@@ -40,3 +40,4 @@ if __name__ == "__main__":
     nums = [0,1,0,3,12,1,123,43,678,34,0,0,0,234,0,4512,5,76,2,45,23,0,0]
     sol = Solution()
     sol.moveZeroes(nums)
+
