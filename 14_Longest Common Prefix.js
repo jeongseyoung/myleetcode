d2f39b826419da7c07dad3c665273f10c1982e75
jeongseyoung <sengangel@naver.com>
2025-09-20 23:57:18 +0900

    Update 14_Longest Common Prefix.js

--- 14_Longest Common Prefix.js
+++ 14_Longest Common Prefix.js
@@ -10,7 +10,7 @@ Example 2:
 Input: strs = ["dog","racecar","car"]
 Output: ""   
 Explanation: There is no common prefix among the input strings
-*/     
+*/          
               
 /*
 배열 첫번째 문자를 앞에서부터 잘라가며(substring) 배열 내에 있는 문자들과 비교한다. 
@@ -50,6 +50,7 @@ strs = ["flower", "flow", "flight"]; //"fl"
 //strs = ["flower", "flower", "flower", "flower"];
 //strs = ["c", "acc", "ccc"]; //""
 console.log("rrrrr", longestCommonPrefix(strs));
+
 
 
 
