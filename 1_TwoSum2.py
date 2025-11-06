180b79bcce93072820b1f7d0c7b2a75f49634614
jeongseyoung <sengangel@naver.com>
2025-11-06 14:48:10 +0900

    Update 1_TwoSum2.py

--- 1_TwoSum2.py
+++ 1_TwoSum2.py
@@ -6,7 +6,7 @@ for문을 한번만 돌리는 게 핵심.
    2. 배열을 꼭 value값에 넣어야하는 건 아님
 """       
                                                        
-                              
+                                  
 def hash_test(nums,target):
     dic:dict = {}
     result:list = []
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     nums = [2,7,11,15]
     target = 9
     hash_test(nums,target)
+
 
 
 
