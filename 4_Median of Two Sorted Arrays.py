214672f348c01c392c4feec48a9f74ee12e8c8cd
jeongseyoung <sengangel@naver.com>
2025-11-29 23:54:21 +0900

    Refactor median calculation and update comments

--- 4_Median of Two Sorted Arrays.py
+++ 4_Median of Two Sorted Arrays.py
@@ -10,7 +10,7 @@ Example 1:
 Input: nums1 = [1,3], nums2 = [2]
 Output: 2.00000
 Explanation: merged array = [1,2,3] and median is 2.
-
+  
 Example 2:
 Input: nums1 = [1,2], nums2 = [3,4]
 Output: 2.50000
@@ -42,6 +42,7 @@ if __name__ == "__main__":
     nums1, nums2 = [], [2,3] #Expected 2.5
     print(themedian(nums1,nums2))
     
+
 
 
 
