602dd928f89dc5b8faac96fedacaceabdfc83aeb
jeongseyoung <sengangel@naver.com>
2026-01-29 23:55:31 +0900

    Update 4_Median of Two Sorted Arrays.py

--- 4_Median of Two Sorted Arrays.py
+++ 4_Median of Two Sorted Arrays.py
@@ -20,7 +20,7 @@ Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 # 내림floor 올림ceil 반올림round
 def themedian(nums1,nums2) -> float:
     nums1.extend(nums2)
-    nums1.sort() 
+    nums1.sort()    
     if len(nums1) % 2 == 0:
         #median = (len(nums1)) / 2
         #return (nums1[m.floor(median)]+nums1[round(median)]) / 2
@@ -42,6 +42,7 @@ if __name__ == "__main__":
     nums1, nums2 = [], [2,3] #Expected 2.5
     print(themedian(nums1,nums2))
     
+
 
 
 
