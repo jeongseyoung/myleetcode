e448ba3126211903ac023ecafb781eb6d99a6403
jeongseyoung <sengangel@naver.com>
2026-07-18 23:58:50 +0900

    Refactor moveZeroes method for clarity and efficiency
    Refactored the moveZeroes method to improve clarity and efficiency. Removed redundant code and added in-place modification of the nums list.

--- 283_Move Zeroes.py
+++ 283_Move Zeroes.py
@@ -26,6 +26,14 @@ cnt = 0
 1 3 0 12 0        
 1 3 12 0 0
 """     
+"""                         
+0,1,0,3,12
+1 0 0 3 12     
+1 0 3 0 12     
+1 0 3 12 0    
+1 3 0 12 0        
+1 3 12 0 0
+"""   
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
         """
