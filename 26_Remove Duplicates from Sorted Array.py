85e3fec5e03c8ae558d83eb8b03e12e03329a95b
jeongseyoung <sengangel@naver.com>
2026-08-15 00:00:15 +0900

    Update 26_Remove Duplicates from Sorted Array.py

--- 26_Remove Duplicates from Sorted Array.py
+++ 26_Remove Duplicates from Sorted Array.py
@@ -1,7 +1,7 @@
 
 #Input: nums = [1,1,2]
 #Output: 2, nums = [1,2,_]
-
+ #Output: 2, nums = [1,2,_]#Output: 2, nums = [1,2,_]        
 #Input: nums = [0,0,1,1,1,2,2,3,3,4]   
 #Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]    
 class Solution(object):
